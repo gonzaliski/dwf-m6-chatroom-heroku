7e4f7ab24fb21ca8c9c4f187117a0ed0f47eb608
Gon <gon.mendez13@gmail.com>
2022-06-02 23:26:06 -0300

    fix state

--- src/state.ts
+++ src/state.ts
@@ -1,7 +1,7 @@
 import {rtdb} from "./rtdb"
 import {map} from "lodash"
-
-const API_BASE_URL ="https://dwf-m6-chatroom.herokuapp.com"
+//""
+const API_BASE_URL = "https://dwf-m6-chatroom.herokuapp.com"
 
 const state = {
   data: {
@@ -54,28 +54,6 @@ const state = {
     cs.roomId = roomId
     this.setState(cs);
   },
-  createUser() {
-    const cs = this.getState();
-    if (cs.email) {
-      fetch(`${API_BASE_URL}/signup`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name: cs.name, email: cs.email }),
-      })
-        .then(data => {
-          return data.json();
-        })
-        .then(res => {
-          cs.userId = res.id;
-          this.setState(cs);  
-        })
-      }else {
-      alert("Debes colocar un mail.");
-        }
-
-  },
   signIn(callback) {
     const cs = this.getState();
     if (cs.email) {
@@ -84,7 +62,7 @@ const state = {
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify({ email: cs.email }),
+        body: JSON.stringify({ email: cs.email, name: cs.fullName }),
       })
         .then((res) => {
           return res.json();
